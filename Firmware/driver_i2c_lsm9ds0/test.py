1e49dc2e02908382fa874d2fc56f091eaa5fb136
ydethe <498517+ydethe@users.noreply.github.com>
2018-10-12 14:31:16 +0200

    Amelioration test.py

--- Firmware/driver_i2c_lsm9ds0/test.py
+++ Firmware/driver_i2c_lsm9ds0/test.py
@@ -1,5 +1,13 @@
+import sys
 import imu_driver
 
 
 a = imu_driver.LSM9DS0()
-print(a.read())
+
+log = open('data.txt', 'w')
+
+for _ in range(sys.argv[1]):
+  r,p,y = a.read()
+  log.write("%f,%f,%f\n" % (r,p,y))
+  
+log.close()
