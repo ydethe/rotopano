ca66084b0b71e2a1c84310183043925cff0056d0
ydethe <498517+ydethe@users.noreply.github.com>
2018-10-17 10:57:25 +0200

    MAJ IHM

--- Firmware/RPFirmware/app.py
+++ Firmware/RPFirmware/app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template
-from flask_wtf import Form
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
-class EmailPasswordForm(Form):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    
-    
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-   form = EmailPasswordForm()
-   if form.validate_on_submit():
-      print("OK")
+@app.route("/")
+@app.route("/control")
+def control():
+   if 'action' in request.args.keys():
+      print("action : %s" % (request.args['action']))
+      
    return render_template('index.html')
+
+@app.route("/config", methods=['GET', 'POST'])
+def config():
+   if request.method == "POST":
+      for key in request.form.keys():
+         for value in request.form.getlist(key):
+            print(key,":",value)
+      return redirect('/control')
+      
+   return render_template('config.html')
    
