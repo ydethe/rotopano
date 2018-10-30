6816f06d80aa643f9c14f3fb5fcbead48b9aa087
ydethe <498517+ydethe@users.noreply.github.com>
2018-10-30 12:35:01 +0100

    Ajout d'un main pour lancer l'appli

---
+++ Firmware/main.py
@@ -0,0 +1,23 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+import tornado
+
+# add your project directory to the sys.path
+project_home = u'.'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+from RPFirmware.app import make_app
+
+
+application = make_app()
+
+application.listen(8888)
+tornado.ioloop.IOLoop.current().start()
+
